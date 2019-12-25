a0886c84fb60ada74855f8be3868b72f35df63bf
Tyler Browning <josephtylerbrowning@gmail.com>
2019-12-25 18:26:57 -0500

    Add deployment and gather scripts.

---
+++ deploy_config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Atom
+cp atom/config.cson $HOME/.atom/
+
+# Bash
+cp bash/.bashrc $HOME/
+cp bash/.bash_profile $HOME/
+
+# Bin folder
+cp -R bin $HOME/
+
+# Iterm2
+# Nothing to do.
---
+++ gather_config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Atom
+cp $HOME/.atom/config.cson atom/
+
+# Bash
+cp $HOME/.bashrc bash/
+cp $HOME/.bash_profile bash/
+
+# Bin folder
+cp -R $HOME/bin/* bin/
+
+# Iterm2
+# Nothing to do.
